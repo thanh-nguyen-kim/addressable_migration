4ece0e625eeaa7a87b8db1dc0cfa93f90caed646
KamperTee <thanhnk@cscmobi.com>
2020-07-31 17:55:32 +0700

    Initial commit

---
+++ Assets/Scripts/SceneLoading.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+public class SceneLoading : MonoBehaviour
+{
+    public string sceneName;
+    public void OnClick()
+    {
+        Addressables.LoadSceneAsync(sceneName);
+    }
+}
